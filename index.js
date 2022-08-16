8f378d6265e5e373aec502194bef03721e90eab0
Chan Lee <clee09818@gmail.com>
2022-08-16 00:14:19 -0400

    commit tree 7fc106666c3796a3e790c4e5c146cb749e15d136

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, str){
+    let list = array.filter(driver => driver.toLowerCase() == str.toLowerCase());
+    return list;
+}
+
+function fuzzyMatch(array, str){
+    let list = array.filter(driver => driver.substring(0,2) == str);
+    return list;
+}
+
+function matchName(array, str){
+    let list = array.filter(driver => driver.name == str );
+    return list;
+}
